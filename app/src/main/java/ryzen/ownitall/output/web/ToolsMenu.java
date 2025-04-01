96a799cca22efb0990276255b14d5dd2de745b18
ryzen <kobe@franssen.xyz>
2025-04-01 18:40:42 -0400

    unarchive menu, and it will be the template for settings menu

--- app/src/main/java/ryzen/ownitall/output/web/ToolsMenu.java
+++ app/src/main/java/ryzen/ownitall/output/web/ToolsMenu.java
@@ -1,8 +1,13 @@
 package ryzen.ownitall.output.web;
 
+import java.io.File;
+import java.util.LinkedHashMap;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ryzen.ownitall.Credentials;
 import ryzen.ownitall.Storage;
@@ -17,14 +22,25 @@ public class ToolsMenu {
     }
 
     @PostMapping("/tools/archive")
-    public String optionArhive() {
+    public String optionArchive() {
         Storage.load().archive();
         return "redirect:/tools";
     }
 
+    @PostMapping("/tools/unarchive/choose")
+    public String optionUnArchive(Model model) {
+        Storage storage = Storage.load();
+        LinkedHashMap<String, String> archiveFoldersMap = new LinkedHashMap<>();
+        for (File file : storage.getArchiveFolders()) {
+            archiveFoldersMap.put(file.getName(), file.getAbsolutePath());
+        }
+        model.addAttribute("archiveFolders", archiveFoldersMap);
+        return "unarchiving";
+    }
+
     @PostMapping("/tools/unarchive")
-    public String optionUnArchive() {
-        // TODO: unarchive menu
+    public String unarchive(@RequestParam("folderPath") String folderPath) {
+        Storage.load().unArchive(new File(folderPath));
         return "redirect:/tools";
     }
 
